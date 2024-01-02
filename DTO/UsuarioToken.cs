2de6de53839df06af53140e3bd287daf97878f53
LAzaro <Lazaro.11.gabriel@gmail.com>
2024-01-02 11:00:50 -0300

    Tentando corrigir AutorizaController

--- DTO/UsuarioToken.cs
+++ DTO/UsuarioToken.cs
@@ -7,7 +7,7 @@ namespace hoop_2.DTO
 {
     public class UsuarioToken
     {
-        public bool Athenticated {get; set;}
+        public bool Authenticated {get; set;}
         public DateTime Expiration {get; set;}
         public string Token {get; set;}
         public string Message {get; set;}
